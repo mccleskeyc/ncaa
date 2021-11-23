06d247729655a0f0b26cfbfe449b7c289d2460fc
mccleskeyc <mccleskeyc@gmail.com>
2021-11-23 11:19:25 -0500

    initial fetch request set up

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,23 +1,31 @@
-import React, { Component } from 'react'
 
-export class Search extends Component {
-    render() {
+
+export default function Search(){
+    const SearchTeams = async (e) => {
+        e.preventDefault();
+        console.log("submitting");
+
+        const query = "Florida State"
+        const url = `https://api.collegefootballdata.com/games/media?year=2021&team=${query}`;
+        const res = await fetch(url);
+        const data  = await res.json();
+        console.log(data);
+    }
+
+
         return (
+
             <div>
-
-
-        <form className="form">
-            <input className="search" type="text" name="query"
-                placeholder="Enter your team name here"/>
-                <br />
-                <br />
-            <button className="button" type="submit">Search</button>
-        </form>
-
-            
+                <form className="form" onSubmit={SearchTeams}>
+                    <input className="search" type="text" name="query"
+                        placeholder="Enter your team name here" />
+                    <br />
+                    <br />
+                    <button className="button" type="submit">Search</button>
+                </form>
             </div>
         )
     }
-}
 
-export default Search
+
+
