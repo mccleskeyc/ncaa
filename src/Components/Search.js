13cbde37c523cb11fa4ab566b06501247185d518
mccleskeyc <mccleskeyc@gmail.com>
2021-11-23 11:30:11 -0500

    updated search button hover color

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,22 +1,22 @@
 
 
 export default function Search(){
-    const SearchTeams = async (e) => {
-        e.preventDefault();
-        console.log("submitting");
+    // const SearchTeams = async (e) => {
+    //     e.preventDefault();
+    //     console.log("submitting");
 
-        const query = "Florida State"
-        const url = `https://api.collegefootballdata.com/games/media?year=2021&team=${query}`;
-        const res = await fetch(url);
-        const data  = await res.json();
-        console.log(data);
-    }
+    //     const query = "Florida State"
+    //     const url = `https://api.collegefootballdata.com/games/media?year=2021&team=${query}`;
+    //     const res = await fetch(url);
+    //     const data  = await res.json();
+    //     console.log(data);
+    // }
 
 
         return (
 
             <div>
-                <form className="form" onSubmit={SearchTeams}>
+                <form className="form" >
                     <input className="search" type="text" name="query"
                         placeholder="Enter your team name here" />
                     <br />
